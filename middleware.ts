e07391a57aba14bd17cff57540e2f68f1a68ad2a
Naga Nithin <connect.naganithin@gmail.com>
2025-01-19 15:19:10 +0530

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,23 +1,10 @@
-// import { authMiddleware } from "@clerk/nextjs";
+import { authMiddleware } from "@clerk/nextjs";
  
-// export default authMiddleware({
-//   publicRoutes: ['/', '/api/webhooks/clerk', '/api/webhooks/stripe']
-// });
- 
-// export const config = {
-//   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
-// };
-
-import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
-
-const isProtectedRoute = createRouteMatcher(["/", "/credits(.*)"]);
-
-export default clerkMiddleware((auth, req) => {
-  if (isProtectedRoute(req)) {
-    auth().protect();
-  }
+export default authMiddleware({
+  // publicRoutes: ['/', '/api/webhooks/clerk', '/api/webhooks/stripe']
+ publicRoutes: ['/', '/credits(.*)']
 });
-
+ 
 export const config = {
-  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
